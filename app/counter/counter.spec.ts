2c57a19674d19f0fe1bae16f1d9fa1fc42b659ee
Pau Torres <CE9@costaisa.org>
2017-04-19 17:36:09 +0200

    created app folder and files

---
+++ app/counter/counter.spec.ts
@@ -0,0 +1,29 @@
+import { Counter } from './counter';
+
+describe('Counter', () => {
+    let counter: Counter;
+
+    beforeEach(() => {
+        counter = new Counter;
+    })
+
+    it('should not fucking crash', ()=>{
+        expect(true).toBe(true);
+    })
+
+    it('should increase value of counter when incremented', (done) => {
+        counter.increment();
+        counter.value.subscribe(value => {
+            expect(value).toEqual(1);
+            done();
+        });
+    });
+
+    it('should decrease value of counter when decremented', (done) => {
+        counter.decrement();
+        counter.value.subscribe(value => {
+            expect(value).toEqual(-1); 
+            done();
+        })
+    })
+})
